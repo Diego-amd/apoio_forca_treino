5f65063fb21b5ff81f9f8a064a94a7b072416490
Diego Armindo <diego.armindo@newm.com.br>
2022-05-04 23:28:27 -0300

    Tela de login quase pronta

--- lib/view/login.view.dart
+++ lib/view/login.view.dart
@@ -23,42 +23,72 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              onSaved: (value) => email = value!,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Campo e-mail obrigatório";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              obscureText: true,
-              onSaved: (value) => senha = value!,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Campo senha obrigatório";
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () => save(context),
-              child: Text("Entrar"),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/register');
-              },
-              child: Text("Registrar"),
-            ),
-          ],
-        ),
-      ),
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage("images/login.png")),
+          ),
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 1,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    stops: [0.3, 1],
+                    colors: [
+                      Color.fromRGBO(6, 32, 41, 2),
+                      Color.fromARGB(0, 32, 41, 2),
+                    ],
+                  )),
+                ),
+              ),
+              const SizedBox(
+                  child: Text("BODY GOALS WORKOUT",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20,
+                      )))
+            ],
+          )),
+      // body: Form(
+      //   key: formKey,
+      //   child: Column(
+      //     children: [
+      //       TextFormField(
+      //         onSaved: (value) => email = value!,
+      //         validator: (value) {
+      //           if (value!.isEmpty) {
+      //             return "Campo e-mail obrigatório";
+      //           }
+      //           return null;
+      //         },
+      //       ),
+      //       TextFormField(
+      //         obscureText: true,
+      //         onSaved: (value) => senha = value!,
+      //         validator: (value) {
+      //           if (value!.isEmpty) {
+      //             return "Campo senha obrigatório";
+      //           }
+      //           return null;
+      //         },
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: () => save(context),
+      //         child: Text("Entrar"),
+      //       ),
+      //       TextButton(
+      //         onPressed: () {
+      //           Navigator.of(context).pushNamed('/register');
+      //         },
+      //         child: Text("Registrar"),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
