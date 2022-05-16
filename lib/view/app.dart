8dda2a3c210119177bec5ac2a45d2f9c0a53d85e
heiterpm <69818662+heiterpm@users.noreply.github.com>
2022-05-15 21:59:15 -0300

    Tela de login 90%

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 
 import 'login.view.dart';
 import 'alteracaoSenha.view.dart';
+import 'homeprofessor.dart';
 
 class App extends StatelessWidget {
   final auth = FirebaseAuth.instance;
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(routes: {
-      '/': (context) => LoginView(),
-      '/alterarSenha': (context) => AlteracaoSenha(),
-    }, initialRoute: '/');
+    return MaterialApp(
+        home: auth.currentUser == null ? LoginView() : LoginView(),
+        routes: {
+          '/login': (context) => LoginView(),
+          '/homeprofessor': (context) => HomeProfessor(),
+          '/alterarSenha': (context) => AlteracaoSenha(),
+        },
+        initialRoute: '/');
   }
 }
