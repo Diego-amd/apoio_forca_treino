7efbd2af2eb777517744dd52bb851d0699c8f26f
Arthur Lins Beluci <arthurjbeluci@gmail.com>
2022-05-27 22:56:50 -0300

    Cadastro de exercicio arrumado

--- lib/view/cad.exercicio.view.dart
+++ lib/view/cad.exercicio.view.dart
@@ -14,6 +14,8 @@ class CadastroExercicio extends StatelessWidget {
         .where('excluido', isEqualTo: false)
         .snapshots();
     return Scaffold(
+      //Background e appBar
+
       appBar: AppBar(
           title: Text("Cadastrar Exercicio",
               style: TextStyle(
@@ -22,100 +24,148 @@ class CadastroExercicio extends StatelessWidget {
                   color: Color.fromARGB(255, 255, 255, 255))),
           backgroundColor: Color.fromARGB(253, 14, 57, 71)),
       body: Container(
-      
- 
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/cadastro.png"), fit: BoxFit.fill),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/cadastrod.png"), fit: BoxFit.fill),
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            stops: [0.6, 1],
+            colors: [
+              Color.fromRGBO(6, 32, 41, 2),
+              Color.fromARGB(0, 32, 41, 2),
+            ],
+          ),
+        ),
 
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  stops: [0.6, 1],
-                  colors: [
-                    Color.fromRGBO(6, 32, 41, 2),
-                    Color.fromARGB(0, 32, 41, 2),
-                  ],
-                  
-                ),
-              ),
-       
-        
-       
+        //Backend cadastro de exercicios
 
         child: StreamBuilder(
+          stream: snapshots,
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
 
-        stream: snapshots,
-        builder: (
-          BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot,
-        ) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
+            if (snapshot.data!.docs.length == 0) {
+              return Center(
+                  child: Container(
+                      child: Text(
+                'Nenhum exercicio ainda',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
+              )));
+            }
 
-          if (snapshot.data!.docs.length == 0) {
-            return Center(
-                child: Container(
-                    child: Text(
-                      'Nenhum exercicio ainda',
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
-                    )));
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (BuildContext context, int i) {
-              var doc = snapshot.data!.docs[i];
-              var value = doc.data;
-              print('exercicios/${doc.reference.id}');
-              return Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 233, 233, 233),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: const EdgeInsets.all(5),
-                child: ListTile(
-                  title: Text(doc['nome']),
-                  subtitle: Text(doc['descricao']),
-                  trailing: CircleAvatar(
-                    backgroundColor: Color.fromARGB(255, 233, 233, 233),
-                    foregroundColor: Color.fromARGB(255, 211, 230, 0),
-                    child: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () => {                           
-                               showDialog(
-                                context: context,
-                                builder: (ctx) => AlertDialog(
-                                  title: Text("Exclusão"),
-                                  content: Text("Voce deseja realmente excluir este exercicio? Caso não queira, clicar fora da caixa de texto."),
-                                  actions: <Widget>[
-                                  
-                                  TextButton(onPressed: () => doc.reference.update({
-                        'excluido': true,
-                      }), 
-                      child: Text('Sim')
-                      ) 
-                                  ]
-                              ),
+            //lista de exercicios exibidos
+
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int i) {
+                var doc = snapshot.data!.docs[i];
+                var value = doc.data;
+                print('exercicios/${doc.reference.id}');
+                return Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 233, 233, 233),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    margin: const EdgeInsets.all(5),
+                    child: ListTile(
+                      title: Text(doc['nome']),
+                      
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                        Text(doc['descricao']),
+                        Text(doc['tipo'],
+                            style: TextStyle(
+                              fontWeight: FontWeight. bold
+                            ),),
+                      ],
+                    ),
+
+                      trailing: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 233, 233, 233),
+                        foregroundColor: Color.fromARGB(255, 224, 0, 0),
+
+                        //Botão de excluir os exercicios
+
+                        child: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () => {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                    contentPadding: EdgeInsets.only(top: 5.0),
+                                    backgroundColor:
+                                        Color.fromARGB(255, 15, 19, 100),
+                                    title: Text(
+                                      "Voce deseja realmente excluir este exercicio?",
+                                      style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255),
+                                          fontSize: 20),
+                                    ),
+                                    content: Text(""),
+                                    actions: <Widget>[
+                                      SizedBox(
+                                        child: TextButton(
+                                          child: Container(
+                                            child: Text(
+                                              'Cancelar',
+                                              style: TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 255, 255, 255),
+                                                  fontSize: 15),
+                                            ),
+                                          ),
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        child: TextButton(
+                                            child: Container(
+                                              child: Text(
+                                                'Sim',
+                                                style: TextStyle(
+                                                    color: Color.fromARGB(
+                                                        255, 255, 255, 255),
+                                                    fontSize: 15),
+                                              ),
+                                            ),
+                                            onPressed: () async {
+                                              doc.reference.update({
+                                                'excluido': true,
+                                              });
+
+                                              Navigator.of(context).pop();
+                                            }),
+                                      ),
+                                    ]);
+                              },
                             ),
                           },
-                          
-                            
                         ),
-                  ),
-                ),
-              );
-            },
-          );
-        },
+                      ),
+                    ));
+              },
+            );
+          },
+        ),
       ),
-      
-      ),
+
+      //Botão de adcionar exercicio
+
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 211, 230, 0),
         onPressed: () => modalCreate(context),
@@ -125,29 +175,39 @@ class CadastroExercicio extends StatelessWidget {
     );
   }
 
+  //Modal do cadastro de exercicio
+
   modalCreate(BuildContext context) {
     var form = GlobalKey<FormState>();
     var nome = TextEditingController();
     var foto = TextEditingController();
     var descricao = TextEditingController();
+    var tipo = TextEditingController();
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          backgroundColor: Color.fromARGB(255, 82, 83, 89),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16.0))),
+          backgroundColor: Color.fromARGB(255, 28, 37, 90),
           title: Text('Criar novo exercicio',
               style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.w700,
                   color: Color.fromARGB(255, 255, 255, 255))),
+
+          //Caixas de texto Modal
+
           content: Form(
             key: form,
             child: Container(
-              height: MediaQuery.of(context).size.height / 3,
+              height: 350,
+              width: 20,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
+                  SizedBox(height: 10),
                   Text('Nome',
                       style: TextStyle(
                           fontSize: 14,
@@ -155,14 +215,15 @@ class CadastroExercicio extends StatelessWidget {
                           color: Color.fromARGB(255, 255, 255, 255))),
                   TextFormField(
                     style: TextStyle(
-                        fontWeight: FontWeight.normal, color: Color.fromARGB(255, 0, 0, 0)),
+                        fontWeight: FontWeight.normal,
+                        color: Color.fromARGB(255, 0, 0, 0)),
                     decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
                       hintText: ('Ex.: Agachamento'),
                       enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color.fromARGB(255, 234, 255, 3)),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 255, 255, 255)),
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
@@ -175,17 +236,37 @@ class CadastroExercicio extends StatelessWidget {
                     },
                   ),
                   SizedBox(height: 20),
-                  /*Text('Foto',
+                  Text('Tipo',
                       style: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.w700,
                           color: Color.fromARGB(255, 255, 255, 255))),
-                  ButtonWidget(
-                    text: 'Select File',
-                    icon: Icons.attach_file,
-                    onClicked: ,
-                  ),*/
-                  SizedBox(height: 20),
+                  TextFormField(
+                     style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Color.fromARGB(255, 0, 0, 0)),
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: ('Ex.: Costa, Peito, Ombro  '),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 255, 255, 255)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Este campo não pode ser vazio';
+                      }
+                      return null;
+                    },
+                    controller: tipo,
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 40,
+                  ),
                   Text('Descrição',
                       style: TextStyle(
                           fontSize: 14,
@@ -193,16 +274,17 @@ class CadastroExercicio extends StatelessWidget {
                           color: Color.fromARGB(255, 255, 255, 255))),
                   TextFormField(
                     style: TextStyle(
-                        fontWeight: FontWeight.normal, color: Color.fromARGB(255, 0, 0, 0)),
+                        fontWeight: FontWeight.normal,
+                        color: Color.fromARGB(255, 0, 0, 0)),
                     decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
                       contentPadding: const EdgeInsets.symmetric(
-                          vertical: 55.0, horizontal: 10.0),
+                          vertical: 35.0, horizontal: 10.0),
                       hintText: '(Opcional)',
                       enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color.fromARGB(255, 234, 255, 3)),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 255, 255, 255)),
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
@@ -212,10 +294,13 @@ class CadastroExercicio extends StatelessWidget {
               ),
             ),
           ),
+
+          //Botões do Modal
+
           actions: <Widget>[
             SizedBox(
               height: 40,
-              width: 100,
+              width: 125,
               child: TextButton(
                 child: Container(
                   child: Text(
@@ -233,7 +318,7 @@ class CadastroExercicio extends StatelessWidget {
             ),
             SizedBox(
               height: 40,
-              width: 100,
+              width: 125,
               child: TextButton(
                 onPressed: () async {
                   if (form.currentState!.validate()) {
@@ -243,6 +328,7 @@ class CadastroExercicio extends StatelessWidget {
                       'nome': nome.text,
                       'descricao': descricao.text,
                       'foto': foto.text,
+                      'tipo': tipo.text,
                       'excluido': false,
                     });
 
