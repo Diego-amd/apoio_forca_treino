fa50d887b3f568f76177f229f293b68b76ade27e
heiterpm <69818662+heiterpm@users.noreply.github.com>
2022-06-15 12:05:59 -0300

    Delete professor

--- lib/view/admin/prof.item.dart
+++ lib/view/admin/prof.item.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
-import '../../model/aluno.model.dart';
 import '../../model/professor.model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfessorItem extends StatelessWidget {
   final ProfessorModel model;
+  final firestore = FirebaseFirestore.instance;
 
   ProfessorItem(this.model);
+
+  void ExcluirProfessor(BuildContext context) async {
+    final QuerySnapshot<Map<String, dynamic>> resultado = await Future.value(
+        firestore
+            .collection("professores")
+            .where("email", isEqualTo: model.email)
+            .get());
+    final List<DocumentSnapshot> documents = resultado.docs;
+
+    if (documents.length == 1) {
+      firestore.collection("professores").doc(resultado.docs[0].id).delete();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +51,74 @@ class ProfessorItem extends StatelessWidget {
                   ),
                 ]),
           ),
+          IconButton(
+            onPressed: () => showDialogAlert(context),
+            icon: const Icon(Icons.delete),
+            color: Colors.red,
+          ),
         ],
       ),
+    );
+  }
+
+  void showDialogAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.only(top: 5.0),
+          backgroundColor: Colors.white,
+          title: Column(
+            children: [
+              const Text(
+                "Você deseja realmente excluir o professor: \n",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                model.nomeCompleto!.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 0),
+              child: TextButton(
+                child: const Text(
+                  'Cancelar',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 0),
+              child: TextButton(
+                child: const Text(
+                  'Sim',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+                onPressed: () => {
+                  ExcluirProfessor(context),
+                  Navigator.of(context).pop(),
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
